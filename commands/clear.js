5798ff340d51d3f672958208772eaba1d56398e6
Asthriona <im@asthriona.com>
2019-08-17 16:15:40 +0200

    change comments on commands handler

--- commands/clear.js
+++ commands/clear.js
@@ -2,11 +2,13 @@ var discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     //clear
-    if(!message.member.hasPermission(MANAGE_MESSAGES)) return message.reply("No.");
-    if(!args[0]) return message.channel.send("oof");
-    message.channel.bulkDelete(args[0]).then(() => {
-        message.channel.send(`Cleared ${args[0]} messages.`).then(msg => msg.delete(5000));
-    });
+//    if(!message.member.hasPermission(MANAGE_MESSAGES)) return message.reply("No.");
+//    if(!args[0]) return message.channel.send("oof");
+//    message.channel.bulkDelete(args[0]).then(() => {
+//        message.channel.send(`Cleared ${args[0]} messages.`).then(msg => msg.delete(5000));
+//    });
+console.log(`${message.author.username} used !clear`)
+        return message.channel.send("Error: Can't clear message for now.");
 }
 
 module.exports.help = {
--- commands/say.js
+++ commands/say.js
@@ -2,7 +2,10 @@ var discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     //say
-
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("No.");
+    let botmessage = args.join(" ");
+    message.delete().catch();
+    message.channel.send(botmessage);
 }
 
 module.exports.help = {
