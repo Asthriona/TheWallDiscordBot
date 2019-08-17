86c49064e3b436768b1ca28ef48f3a1b4d12ddf4
Asthriona <im@asthriona.com>
2019-08-17 13:53:45 +0200

    Add lot of commands and commands handler

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 var botconfig = require("./botconfig.json");
 var discord = require("discord.js");
+var fs = require("fs");
 
-var bot = new discord.Client({disableEveryone: true})
+var bot = new discord.Client({disableEveryone: true});
+bot.commands = new discord.Collection();
+
+fs.readdir("./commands", (err, files) => {
+    if(err) console.log(err);
+
+    let jsfile = files.filter(f => f.split(".").pop() === "js")
+    if(jsfile.length <= 0){
+        console.log("Couldn't find commands.");
+        return;
+    }
+    console.log("Loading files...");
+    jsfile.forEach((f, i) =>{
+        let props = require(`./commands/${f}`);
+        console.log(`${f} loaded!`);
+        bot.commands.set(props.help.name, props);
+    })
+})
 
 bot.on("ready", async () => {
     console.log(`Bot is online!`);
-    bot.user.setActivity("Developement in progress...")
+    bot.user.setActivity("Update...", {type: "WATCHING"});
 });
 
 bot.on("message", async message => {
@@ -16,11 +34,47 @@ bot.on("message", async message => {
     let messageArray = message.content.split(" ");
     let cmd = messageArray[0];
     let args = messageArray.slice(1);
-
-    //!say hello
+//Commands
+    //!test = Hello World
     if(cmd === `${prefix}test`){
         return message.channel.send("Hello World!");
     }
+    //!test = Hello World
+    if(cmd === `${prefix}ping`){
+        return message.channel.send("Pong");
+    }
+    if(cmd === `${prefix}praise`){
+        return message.channel.send("You praised The Wall. I will protect you!");
+    }
+    //botinfo
+    if(cmd === `${prefix}info`){
+        let bicon = bot.user.displayAvatarURL;
+        let botembed = new discord.RichEmbed()
+        .setDescription("Bot Information")
+        .setColor("#800080")
+        .addField("Bot Name", bot.user.username)
+        .addField("Version:", "1.0.0")
+        .addField("Developped by", "Asthriona")
+        .addField("Created on", bot.user.createdAt)
+        .setThumbnail(bicon);
+        return message.channel.send(botembed)
+    }
+
+    if(cmd === `${prefix}server`){
+        let sicon = message.guild.iconURL;
+        let serverembed = new discord.RichEmbed()
+        .setDescription("Server Information")
+        .setColor("#800080")
+        .setThumbnail(sicon)
+        .addField("Server Name", message.guild.name)
+        .addField("Created on", message.guild.createdAt)
+        .addField("You joined", message.member.joinedAt)
+        .addField("Total members", message.guild.memberCount)
+        return message.channel.send(serverembed);
+    }
+
+    let commandfile = bot.commands.get(cmd.slice(prefix.length));
+    if(commandfile) commandfile.run(bot,message,args);
 });
 
 bot.login(botconfig.token)
