a18196c085a8e31f93ab6a9b498f7160b1b5ba0f
Asthriona <im@asthriona.com>
2019-10-10 15:59:19 +0200

    update logging on !say

--- commands/say.js
+++ commands/say.js
@@ -2,11 +2,11 @@ var discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     //say
-    console.log(`${message.author.username} used !say`)
+    logger.info(`${message.author.username} used !say`)
     if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("No.");
     let botmessage = args.join(" ");
     message.delete().catch();
-    console.log(`${message.author.username} -> ${botmessage}`)
+    logger.info(`${message.author.username} -> ${botmessage}`)
     message.channel.send(botmessage);
 }
 
