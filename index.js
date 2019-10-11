51fd7301a47186faecf328dec4ef7308e9672f9b
Asthriona <im@asthriona.com>
2019-10-11 04:08:16 +0200

    add a server owner in !server

--- index.js
+++ index.js
@@ -194,10 +194,11 @@ bot.on("message", async message => {
         .addField("Server Name", message.guild.name)
         .addField("Created on", message.guild.createdAt)
         .addField("You joined", message.member.joinedAt)
+        .addField("Server Owner", message.guild.owner)
         .addField("Total members", message.guild.memberCount)
         return message.channel.send(serverembed);
     }
-    
+
     //emotes
     let eUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     //hug
