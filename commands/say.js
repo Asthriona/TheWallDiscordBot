fc6e69aa5a6130f68f6393e12f6e6be340803e08
Asthriona <im@asthriona.com>
2019-10-23 08:51:22 +0200

    hum... That's akward.

---
+++ commands/say.js
@@ -0,0 +1,15 @@
+var discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    //say
+    logger.info(`${message.author.username} used !say`)
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("No.");
+    let botmessage = args.join(" ");
+    message.delete().catch();
+    logger.info(`${message.author.username} -> ${botmessage}`)
+    message.channel.send(botmessage);
+}
+
+module.exports.help = {
+    name: "say"
+}
---
+++ commands/shutdown.js
@@ -0,0 +1,17 @@
+var discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    if(message.author.id != "186195458182479874") return message.channel.send("You cant shutdown the bot.")
+
+    try{
+        await message.channel.send("bot is shuting down...")
+        process.exit()
+    } catch(e) {
+        message.channel.send(`ERROR: ${e.message}`)
+
+    }
+}
+
+module.exports.help = {
+    name: "shutdown"
+}
